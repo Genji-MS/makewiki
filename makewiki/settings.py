1813545e2cfdbab01b056ca5bf8a80b155588348
Genji-MS <Genji.Tapia@students.makeschool.com>
2019-12-12 10:40:25 -0800

    deployed onto heroku

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1yct-t!2bnkgc7j59z+9cdd2k)@y+ftqor$!aya()3if^cnlo-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'gt-makewiki-v2.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'gt-makewiki-v2.herokuapp.com']
 
 
 # Application definition
